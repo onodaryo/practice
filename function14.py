e20b07e4cc1342221b1961ea6d911c41378deebe
{ryoonoda} <{onoda.ryo@genetec.co.jp}>
2024-04-15 10:41:42 +0900

    modefy function file

--- function14.py
+++ function14.py
@@ -1,10 +1,7 @@
 import sys
 
 def is_leap_year(year):
-    if year % 4 == 0 and year % 100 != 0:
-        return True
-    else:
-        return False
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
