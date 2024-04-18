8e15023c2d22d1a15f7ff7312c8da5a94324aa6d
ryoonoda <onoda.ryo@genetec.co.jp>
2024-04-18 16:44:53 +0900

    閏年判定関数を論理演算子縛りで書き直しの書き直し

--- go/util/util.go
+++ go/util/util.go
@@ -1,16 +1,13 @@
 package util
 
 func IsLeapYear(year int) bool {
-	if year%400 != 0 {
-		return false
-	} else {
-		return true
-	}
 	if year%4 != 0 {
 		return false
 	} else if year%100 != 0 {
 		return true
-	} else {
+	} else if year%400 != 0 {
 		return false
+	} else {
+		return true
 	}
 }
