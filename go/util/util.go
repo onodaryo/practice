52615d1f5a0223592d585bf9a6694cdf5fc2881c
ryoonoda <166459552+onodaryo@users.noreply.github.com>
2024-04-18 16:22:09 +0900

    Update util.go

--- go/util/util.go
+++ go/util/util.go
@@ -1,14 +1,16 @@
 package util
 
 func IsLeapYear(year int) bool {
-	var check bool = false
-	if year%4 == 0 {
-		if year%100 != 0 {
-			check = true
-		}
+	if year%400 != 0 {
+		return false
+	} else {
+		return true
 	}
-	if year%400 == 0 {
-		check = true
+	if year%4 != 0 {
+		return false
+	} else if year%100 != 0 {
+		return true
+	} else {
+		return false
 	}
-	return check
 }
