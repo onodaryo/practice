8c1d6ffe2e0bd273ac90fde21d5e9d0d2453da31
ryoonoda <onoda.ryo@genetec.co.jp>
2024-04-19 09:59:25 +0900

    testを修正

--- go/util/util_test.go
+++ go/util/util_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestIsLeapYear(t *testing.T) {
+func TestIsLeapYear_useAssert(t *testing.T) {
 	// 4で割り切れる, 100で割り切れる, 400で割り切れる
 	assert.True(t, IsLeapYear(2000), "4で割り切れる, 100で割り切れる, 400で割り切れるtest")
 	assert.False(t, IsLeapYear(1999), "4で割り切れる, 100で割り切れる, 400で割り切れる境界値(-1)test")
@@ -25,4 +25,8 @@ func TestIsLeapYear(t *testing.T) {
 	assert.False(t, IsLeapYear(17), "4で割り切れない, 100で割り切れない, 400で割り切れないtest")
 	assert.True(t, IsLeapYear(16), "4で割り切れない, 100で割り切れない, 400で割り切れない境界値(-1)test")
 	assert.False(t, IsLeapYear(18), "4で割り切れない, 100で割り切れない, 400で割り切れない境界値(+1)test")
+}
+
+func TestMain(m *testing.M) {
+	m.Run()
 }
